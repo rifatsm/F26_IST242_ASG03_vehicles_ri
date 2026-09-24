462ecbe4ce7822449d5ed6c2fd51a0679c093e94
rifatsm <rifatsm@vt.edu>
2026-09-24 14:38:52 -0400

    (feat*) Add manufacturer class.
    Represents a vehicle manufacturer.

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ The main entry point for ASG03 Vehicle Hierarchy with ABC
 Author: Mr. Ri
 """
 
+from manufacturer import Manufacturer
 
 def main():
-    pass
+
+    """
+    (Ford, USA)` F150 in production = True,  release year: 2020, mpg: 20.00 is dually truck: False
+    (Honda, Japan) Civic in production = False,  release year: 1996, mpg: 28.00
+    (BMW, Germany) M3 Limited in production = False,  release year: 2015, mpg: 30.00
+    (Toyota, Ja`pan) Tundra in production = False,  release year: 1987, mpg: 30.00 is dually truck: True
+    """
+    ford = Manufacturer("Ford", "USA")
+    honda = Manufacturer("Honda", "Japan")
+    bmw = Manufacturer("BMW", "Germany")
+    toyota = Manufacturer("Toyota", "Japan")
+
+    print(ford) # printing the object location :/ 
 
 if __name__ == "__main__":
     main()
